703b8892b60275827fedd765e38b65b6025ac4d6
bobwang1979 <wzpbobsir@hotmail.com>
2018-12-27 16:37:43 +0800

    Update MvcConfiguration.java

--- src/main/java/com/amazonaws/labs/sampleapp/MvcConfiguration.java
+++ src/main/java/com/amazonaws/labs/sampleapp/MvcConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.autoscaling.AmazonAutoScaling;
 import com.amazonaws.services.autoscaling.AmazonAutoScalingClient;
 import com.amazonaws.services.codedeploy.AmazonCodeDeploy;
